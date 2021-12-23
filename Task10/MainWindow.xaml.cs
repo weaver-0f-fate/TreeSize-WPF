6ff5b809f5182d0c7546357cec9e5b660bf1b0bb
weavelol <deniss.shutov98@gmail.com>
2021-12-23 19:30:03 +0200

    Models refactoring. Commads Implementation.

--- Task10/MainWindow.xaml.cs
+++ Task10/MainWindow.xaml.cs
@@ -1,71 +1,15 @@
-﻿using Microsoft.Win32;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Forms;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using Task10.CustomControls;
+using ViewModels;
 
 namespace Task10 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
-        private string _selectedFolderPath;
-
         public MainWindow() {
             InitializeComponent();
-        }
-
-        private void SelectFolder(object sender, RoutedEventArgs e) {
-            using var folderDialog = new FolderBrowserDialog(); 
-            
-            var result = folderDialog.ShowDialog();
-            if (result == System.Windows.Forms.DialogResult.OK) {
-                _selectedFolderPath = folderDialog.SelectedPath;
-                FileURL.Text = $" | {folderDialog.SelectedPath}";
-            }
-        }
-
-        private void Analyze(object sender, RoutedEventArgs e) {
-            if (_selectedFolderPath is null) {
-                return;
-            }
-
-            ListDirectory(_selectedFolderPath);
-        }
-
-        private void ListDirectory(string path) {
-            DirectoryTree.Items.Clear();
-            var rootDirectoryInfo = new DirectoryInfo(path);
-            DirectoryTree.Items.Add(CreateDirectoryNode(rootDirectoryInfo));
-        }
-
-        private static TreeViewItem CreateDirectoryNode(DirectoryInfo directoryInfo) {
-            var directoryNode = new TreeViewItem { Header = directoryInfo.Name };
-
-
-            foreach (var directory in directoryInfo.GetDirectories()) {
-                directoryNode.Items.Add(CreateDirectoryNode(directory));
-            }
-
-            foreach (var file in directoryInfo.GetFiles()) {
-                directoryNode.Items.Add(new TreeViewItem() { Header = new TreeItem(file) });
-            }
-
-            return directoryNode;
-
+            DataContext = new ApplicationViewModel();
         }
     }
 }
---
+++ ViewModels/ApplicationViewModel.cs
@@ -0,0 +1,86 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Forms;
+using ViewModels.Commands;
+
+namespace ViewModels {
+    public class ApplicationViewModel : INotifyPropertyChanged {
+
+
+        private string _selectedFolderPath;
+        public string SelectedFolderPath {
+            get {
+                return _selectedFolderPath;
+            } 
+            set {
+                _selectedFolderPath = value;
+                OnPropertyChanged("SelectedFolderPath");
+            } 
+        }
+
+
+
+        #region Commands
+        private RelayCommand _selectFolderCommand;
+        public RelayCommand SelectFolderCommand {
+            get {
+                return _selectFolderCommand ??= new RelayCommand(x => {
+                        using var folderDialog = new FolderBrowserDialog();
+                        var result = folderDialog.ShowDialog();
+                        if (result == DialogResult.OK) {
+                            SelectedFolderPath = folderDialog.SelectedPath;
+                        }
+                    });
+            }
+        }
+
+        private RelayCommand _analyzeFolderCommand;
+        public RelayCommand AnalyzeFolderCommand {
+            get {
+                return _analyzeFolderCommand ??= new RelayCommand(x => { 
+                        //TODO analyzeCommand logic
+                    });
+            }
+        }
+        #endregion
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "") {
+            if (PropertyChanged != null) {
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+            }
+        }
+    }
+}
+/*
+ private void Analyze(object sender, RoutedEventArgs e) {
+            if (_selectedFolderPath is null) {
+                return;
+            }
+
+            ListDirectory(_selectedFolderPath);
+        }
+
+        private void ListDirectory(string path) {
+            DirectoryTree.Items.Clear();
+            var rootDirectoryInfo = new DirectoryInfo(path);
+            DirectoryTree.Items.Add(CreateDirectoryNode(rootDirectoryInfo));
+        }
+
+        private static TreeViewItem CreateDirectoryNode(DirectoryInfo directoryInfo) {
+            var directoryNode = new TreeViewItem { Header = directoryInfo.Name };
+
+
+            foreach (var directory in directoryInfo.GetDirectories()) {
+                directoryNode.Items.Add(CreateDirectoryNode(directory));
+            }
+
+            foreach (var file in directoryInfo.GetFiles()) {
+                directoryNode.Items.Add(new TreeViewItem() { Header = new TreeItem(file) });
+            }
+
+            return directoryNode;
+
+        }
+ */
