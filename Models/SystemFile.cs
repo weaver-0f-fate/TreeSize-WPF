fe9123c0a4175fac2f6ed80ff2f7835e4ecf405c
weavelol <deniss.shutov98@gmail.com>
2021-12-26 14:50:02 +0200

    refactoring

--- Models/SystemFile.cs
+++ Models/SystemFile.cs
@@ -1,36 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.ComponentModel;
 using System.IO;
-using System.Runtime.CompilerServices;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace Models {
-    public class SystemFile : INotifyPropertyChanged{
-        private string _name;
-        private Size _size;
-
-        #region Properties
-        public string Name {
-            get { return _name; }
-            set {
-                _name = value;
-                OnPropertyChanged("Name");
-            }
-        }
-
-        public Size Size {
-            get { return _size; }
-            set {
-                _size = value;
-                OnPropertyChanged("Size");
-            }
-        }
+    public class SystemFile{
+        public string Name { get; set; }
+        public Size Size { get; set; }
         public Enums.FileType Type { get; }
         public ObservableCollection<SystemFile> NestedItems { get; set; }
-        #endregion
+
 
         private SystemFile(string name, double size, Enums.FileType type, ObservableCollection<SystemFile> collection) {
             Name = name;
@@ -65,17 +44,7 @@ namespace Models {
             if (fileInfo is FileInfo f) {
                 return new SystemFile(f.Name, f.Length, Enums.FileType.File, new ObservableCollection<SystemFile>());
             }
-
             throw new Exception();
-        }
-
-
-
-        public event PropertyChangedEventHandler PropertyChanged;
-        public void OnPropertyChanged([CallerMemberName] string prop = "") {
-            if (PropertyChanged != null) {
-                PropertyChanged(this, new PropertyChangedEventArgs(prop));
-            }
         }
     }
 }
