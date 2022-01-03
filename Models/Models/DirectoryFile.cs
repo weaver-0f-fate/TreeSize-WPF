7ba5eed61a83a92f8877ab4ab066ec2e6bee0cb7
weavelol <deniss.shutov98@gmail.com>
2022-01-03 19:00:22 +0200

    Parallel

--- Models/Models/DirectoryFile.cs
+++ Models/Models/DirectoryFile.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Concurrent;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
-using System.Threading.Tasks;
 using System.Windows.Data;
 
 namespace Models {
--- Services/ReadAsyncService.cs
+++ Services/ReadAsyncService.cs
@@ -1,26 +1,25 @@
 ﻿using Models;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace Services {
     public static class ReadAsyncService {
-        public static async Task ReadRootDirectoryAsync(DirectoryFile rootDirectory) {
-            await ReadDirectoryAsync(rootDirectory);
+        public static void ReadRootDirectoryAsync(DirectoryFile rootDirectory) {
+            ReadDirectoryAsync(rootDirectory);
             rootDirectory .LoadNestedFiles();
         }
-        private static async Task ReadDirectoryAsync(DirectoryFile rootDirectory) {
+        private static void ReadDirectoryAsync(DirectoryFile rootDirectory) {
             rootDirectory.LoadNestedDirectories();
-            var tasks = rootDirectory.NestedItems.Select(nestedDirectory => ReadDirectoryAsync(rootDirectory, (DirectoryFile)nestedDirectory));
-            await Task.WhenAll(tasks);
-        }
 
-        private static async Task ReadDirectoryAsync(DirectoryFile rootDir, DirectoryFile dir) {
-            try {
-                await ReadDirectoryAsync(dir);
-                dir.LoadNestedFiles();
-                rootDir.Size += dir.Size;
-            }
-            catch { }
+            Parallel.ForEach(rootDirectory.NestedItems, dir => {
+                if (dir is DirectoryFile dirFile) {
+                    try {
+                        ReadDirectoryAsync(dirFile);
+                        dirFile.LoadNestedFiles();
+                        rootDirectory.Size += dirFile.Size;
+                    }
+                    catch { }
+                }
+            });
         }
     }
 }
